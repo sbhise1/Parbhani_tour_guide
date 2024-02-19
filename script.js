8575abc718c0967c66cc4c1222f02ef7c895d21c
sbhise1 <95121039+sbhise1@users.noreply.github.com>
2024-02-19 13:58:14 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,94 @@
+function removevisi(){
+    let prop = document.querySelectorAll(".find")
+    prop.forEach((e)=>{
+        if(e.classList.contains("visi")==true){
+            e.classList.remove("visi")
+        }else{
+            e.classList.add("visi");
+        }
+        console.log(e)
+    })
+}
+
+function PBcontent(){
+    let card = document.getElementById("pb");
+    if(card.classList.contains("card")){
+        card.classList.remove("card")
+    }else{
+        card.classList.add("card")
+    }
+    
+}
+
+function GKcontent(){
+    let card = document.getElementById("gk");
+    if(card.classList.contains("card")){
+        card.classList.remove("card")
+    }else{
+        card.classList.add("card")
+    }
+    
+}
+function SPcontent(){
+    let card = document.getElementById("sp");
+    if(card.classList.contains("card")){
+        card.classList.remove("card")
+    }else{
+        card.classList.add("card")
+    }
+    
+}
+function PTcontent(){
+    let card = document.getElementById("pt");
+    if(card.classList.contains("card")){
+        card.classList.remove("card")
+    }else{
+        card.classList.add("card")
+    }
+    
+}
+function MNcontent(){
+    let card = document.getElementById("mn");
+    if(card.classList.contains("card")){
+        card.classList.remove("card")
+    }else{
+        card.classList.add("card")
+    }
+    
+}
+function PLcontent(){
+    let card = document.getElementById("pl");
+    if(card.classList.contains("card")){
+        card.classList.remove("card")
+    }else{
+        card.classList.add("card")
+    }
+    
+}
+function SLcontent(){
+    let card = document.getElementById("sl");
+    if(card.classList.contains("card")){
+        card.classList.remove("card")
+    }else{
+        card.classList.add("card")
+    }
+    
+}
+function JNcontent(){
+    let card = document.getElementById("jn");
+    if(card.classList.contains("card")){
+        card.classList.remove("card")
+    }else{
+        card.classList.add("card")
+    }
+    
+}
+function PUcontent(){
+    let card = document.getElementById("pu");
+    if(card.classList.contains("card")){
+        card.classList.remove("card")
+    }else{
+        card.classList.add("card")
+    }
+    
+}
